9f94562481a16fd633f8d7b4e2b53fa3d0fc590c
Vincent Zerbib <contact@zelabstudio.com>
2022-11-29 12:42:48 +0100

    save work

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,12 @@
 const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
 
 exports.register = (req, res) => {
+  let hashedPassword = bcrypt.hashSync(req.body.password, 10);
   const newUser = new User({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
-    password: req.body.password,
+    password: hashedPassword,
     email: req.body.email
   });
   newUser.save()
@@ -14,4 +16,27 @@ exports.register = (req, res) => {
     .catch(err => {
       res.status(400).send(err)
     })
+}
+
+exports.login = (req, res) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({
+          message:"user not found"
+        })
+      }
+      let passwordValid = bcrypt.compareSync(req.body.password, user.password);
+      if (!passwordValid) {
+        return res.status(401).send({
+          message: "password not valid",
+          auth: false
+        })
+      }
+      res.send({
+        message: "User logged",
+        auth:true
+      })
+    })
+  .catch(err=>res.Status(400).send(err))
 }
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const authController = require('../controllers/auth.controller');
 
 router.post('/register', authController.register);
-// router.get('/getme', controller);
-// router.post('/login', controller);
+router.post('/login', authController.login);
 
 module.exports = router;
